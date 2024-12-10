5d44942e46d75fc13a15f4f8d76c0e40a8513647
Anurag-Mishra22 <anurag2025mishra@gmail.com>
2024-12-10 12:32:06 +0530

    changed port

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def index():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="https://virtualkeyboard-egbz.onrender.com", port=5000, debug=True)
